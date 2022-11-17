dc5d301286179393ab79d6fdbc614f808508d500
Mark Dibe <dibemark.job@gmail.com>
2022-11-17 06:36:06 +0100

    Create Operation Service
    Instantiate Operation Service that implements IOperations

---
+++ RPNCalculatorAPI/Services/OperationService.cs
@@ -0,0 +1,27 @@
+﻿using RPNCalculatorAPI.IServices;
+
+namespace RPNCalculatorAPI.Services
+{
+    public class OperationService : IOperations
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Substract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
