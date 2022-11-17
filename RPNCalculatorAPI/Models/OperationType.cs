cc5f4b9b0004d4b2b69b2342af820c9ab06b1dba
Mark Dibe <dibemark.job@gmail.com>
2022-11-17 07:03:10 +0100

    Create OperationType
    an Enum of OperationType that indicates the allowed operations

---
+++ RPNCalculatorAPI/Models/OperationType.cs
@@ -0,0 +1,11 @@
+﻿namespace RPNCalculatorAPI.Models
+{
+    public enum OperationType
+    {
+        Add = 1,
+        Substract = 2,
+        Multiply = 3,
+        Divide = 4,
+        NOT_OPERATOR = 999
+    };
+}
