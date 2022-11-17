226c93fae04c844ccb19fdabb3d5e9e080451dc3
Mark Dibe <dibemark.job@gmail.com>
2022-11-17 07:02:13 +0100

    Create IOperationIdentifier interface
    to identify which operation type need to be in control

---
+++ RPNCalculatorAPI/IServices/IOperationIdentifier.cs
@@ -0,0 +1,9 @@
+﻿using RPNCalculatorAPI.Models;
+
+namespace RPNCalculatorAPI.IServices
+{
+    public interface IOperationIdentifier
+    {
+        public OperationType IdentiftOperationType(string signature);
+    }
+}
