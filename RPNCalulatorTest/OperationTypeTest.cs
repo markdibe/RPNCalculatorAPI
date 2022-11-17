b179eb0b3575bf512a9cdca0bf64d8069c61be90
Mark Dibe <dibemark.job@gmail.com>
2022-11-17 13:54:06 +0100

    Add OperationTypeTest
    Test the functionality of IdentifyOperationType and make sure that it returns the right enum based on the input

---
+++ RPNCalulatorTest/OperationTypeTest.cs
@@ -0,0 +1,59 @@
+﻿using Moq;
+using RPNCalculatorAPI.IServices;
+using RPNCalculatorAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RPNCalulatorTest
+{
+    public  class OperationTypeTest
+    {
+        private readonly Mock<IOperationIdentifier> _identifier;
+        
+        public OperationTypeTest()
+        {
+            _identifier = new Mock<IOperationIdentifier>();
+        }
+
+        [Fact]
+        public void ShouldReturnAddtionType()
+        {
+            string input = "+";
+            _identifier.Setup(x => x.IdentiftOperationType(input)).Returns(OperationType.Add);
+        }
+
+        [Fact]
+        public void ShouldReturnSubstractionType()
+        {
+            string input = "-";
+            _identifier.Setup(x => x.IdentiftOperationType(input)).Returns(OperationType.Substract);
+        }
+
+        [Fact]
+        public void ShouldReturnMultiplicationType()
+        {
+            string input = "*";
+            _identifier.Setup(x => x.IdentiftOperationType(input)).Returns(OperationType.Multiply);
+        }
+
+        [Fact]
+        public void ShouldReturnDivisionType()
+        {
+            string input = "+";
+            _identifier.Setup(x => x.IdentiftOperationType(input)).Returns(OperationType.Divide);
+        }
+
+        [Fact]
+        public void ShouldReturnNotOperatorType()
+        {
+            string input = "5";
+            _identifier.Setup(x => x.IdentiftOperationType(input)).Returns(OperationType.NOT_OPERATOR);
+        }
+
+
+    }
+}
