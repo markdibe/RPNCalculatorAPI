1a2653ac9ca51ffd619ace1e2a5d01943b4e930b
Mark Dibe <dibemark.job@gmail.com>
2022-11-17 06:32:58 +0100

    Create IOperation Interface
    This interface contains the following operations:
    Add - Substract - Multiply - Substract

---
+++ RPNCalculatorAPI/IServices/IOperations.cs
@@ -0,0 +1,10 @@
+﻿namespace RPNCalculatorAPI.IServices
+{
+    public interface IOperations
+    {
+        int Add(int x, int y);
+        int Substract(int x, int y);
+        int Multiply(int x, int y);
+        int Divide(int x, int y);
+    }
+}
