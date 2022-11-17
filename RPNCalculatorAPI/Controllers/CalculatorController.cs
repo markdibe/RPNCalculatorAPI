015fa991aa010770bdc000b92bd155229cea1d6d
Mark Dibe <dibemark.job@gmail.com>
2022-11-17 06:33:28 +0100

    Create CalculatorController.cs
    Instantiate Calculator Controller

---
+++ RPNCalculatorAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace RPNCalculatorAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ConcurrentStack<int> _stack;
+        public CalculatorController()
+        {
+            _stack = new ConcurrentStack<int>();
+        }
+
+        [HttpPost]
+        public void Push([FromBody]string input)
+        {
+
+        }
+
+
+        
+    }
+}
