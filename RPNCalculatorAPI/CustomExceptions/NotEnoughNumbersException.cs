c5896cff29f6bc13cb28edd8ad0bcbf55cdfcff3
Mark Dibe <dibemark.job@gmail.com>
2022-11-17 14:01:52 +0100

    create NotEnoughNumbersException
    custom exception in case the user sent an operator and we do not have 2 element in the stack

---
+++ RPNCalculatorAPI/CustomExceptions/NotEnoughNumbersException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPNCalculatorAPI.CustomExceptions
+{
+    public class NotEnoughNumbersException : Exception
+    {
+        public NotEnoughNumbersException(string message)
+     : base(message)
+        {
+        }
+    }
+}
