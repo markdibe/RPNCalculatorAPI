0ac99e92408c9bcaf13862442256851c5f355f28
Mark Dibe <dibemark.job@gmail.com>
2022-11-17 07:02:46 +0100

    Create OperationIdentitfierService
    A service That Implements IOperationIdentifier

---
+++ RPNCalculatorAPI/Models/OperationIdentifierService.cs
@@ -0,0 +1,33 @@
+﻿using RPNCalculatorAPI.IServices;
+
+namespace RPNCalculatorAPI.Models
+{
+    public class OperationIdentifierService : IOperationIdentifier
+    {
+        public OperationType IdentiftOperationType(string signature)
+        {
+            OperationType operationType = OperationType.NOT_OPERATOR;
+            switch (signature)
+            {
+                case "+":
+                    operationType = OperationType.Add;
+                    break;
+                case "-":
+                    operationType = OperationType.Substract;
+                    break;
+                case "/":
+                    operationType = OperationType.Divide;
+                    break;
+                case "*":
+                    operationType = OperationType.Multiply;
+                    break;
+                default:
+                    operationType = OperationType.NOT_OPERATOR;
+                    break;
+            }
+            return operationType;
+                
+
+        }
+    }
+}
